7371a71c7ed644cf9ea210b7019f8dd86aff5036
jaovfp <joaovitor191124@gmail.com>
2025-03-29 13:34:50 -0300

    Initial project upload

---
+++ app/src/main/kotlin/br/com/ccb/gerenciardjccb/GerenciardjccbApplication.kt
@@ -0,0 +1,11 @@
+package br.com.ccb.gerenciardjccb
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GerenciardjccbApplication
+
+fun main(args: Array<String>) {
+	runApplication<GerenciardjccbApplication>(*args)
+}
